d817a7362bfb257fa7c67e8b16fe3c9ed81378e7
lalitcrane <48549060+lalitcrane@users.noreply.github.com>
2020-06-12 18:36:46 +0530

    Create Ionic4Reference.js

---
+++ Ionic4Reference.js
@@ -0,0 +1,18 @@
+
+
+
+------------------------------------------------------------
+
+Adding Database Service SQLLite
+Setup:
+ 1. Install SQL Storage and SQL Porter
+ 2. Create Service using ionic CLI 
+      $ ionic generate service myservices/database
+ 3. Add Code to muyservices/database.service.ts
+ 
+ Using SQLLite 
+  1. Database Creation - Tables and Initial Data. In Construction of Database Service
+  2. Execue SQL - for SELECT, INSERT or DELETE Operations
+ 
+ Example:
+ https://devdactic.com/ionic-4-sqlite-queries/
